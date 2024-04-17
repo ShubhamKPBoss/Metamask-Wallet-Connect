f1d00b2c1195eb883946928c3f1754ec7f0bc556
Aashish Panthi <aashishpanthi11@gmail.com>
2024-04-17 19:23:48 +0545

    Use the latest code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
+import { ConnectWallet, useAddress} from "@thirdweb-dev/react";
 
 function App() {
   const address = useAddress();
-  const connectWithMetamask = useMetamask();
-  const disconnectWallet = useDisconnect();
+  
+  console.log("address", address);
   return (
     <div>
     <h1> This is Testing ...</h1>
-      {address ? (
-        <>
-          <button onClick={disconnectWallet}>Disconnect Wallet</button>
-          <p>Your address: {address}</p>
-        </>
-      ) : (
-        <button onClick={connectWithMetamask}>Connect with Metamask</button>
-      )}
+      <ConnectWallet />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,50 +1,50 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
-
-// This is the chainId your dApp will work on.
-const activeChainId = ChainId.Mainnet;
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(
-  <React.StrictMode>
-    <ThirdwebProvider desiredChainId={activeChainId}>
-      <App />
-    </ThirdwebProvider>
-  </React.StrictMode>
-);
-
-
 // import React from "react";
-// import ReactDOM from "react-dom/client";
+// import { createRoot } from "react-dom/client";
 // import App from "./App";
-// import {
-//   ThirdwebProvider,
-//   metamaskWallet,
-//   coinbaseWallet,
-//   walletConnect,
-// } from "@thirdweb-dev/react";
+// import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
-// const activeChain = "ethereum";
+// // This is the chainId your dApp will work on.
+// const activeChainId = ChainId.Mainnet;
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
+// const container = document.getElementById("root");
+// const root = createRoot(container);
 // root.render(
-//   <ThirdwebProvider
-//     supportedWallets={[
-//       metamaskWallet({
-//         recommended: true,
-//       }),
-//       coinbaseWallet(),
-//       walletConnect(),
-//     ]}
-//     activeChain={activeChain}
-//     clientId="74a6f099e13f7847edf9cdd9f1fb72b4"
-//   >
-//     <App />
-//   </ThirdwebProvider>
+//   <React.StrictMode>
+//     <ThirdwebProvider desiredChainId={activeChainId}>
+//       <App />
+//     </ThirdwebProvider>
+//   </React.StrictMode>
 // );
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import {
+  ThirdwebProvider,
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnect,
+} from "@thirdweb-dev/react";
+
+const activeChain = "ethereum";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <ThirdwebProvider
+    supportedWallets={[
+      metamaskWallet({
+        recommended: true,
+      }),
+      coinbaseWallet(),
+      walletConnect(),
+    ]}
+    activeChain={activeChain}
+    clientId="74a6f099e13f7847edf9cdd9f1fb72b4"
+  >
+    <App />
+  </ThirdwebProvider>
+);
 
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
